0d80103228e38a4935e921568f91c9c440a404c7
aliicayli <aliicayli@gmail.com>
2023-07-31 13:49:42 +0300

    Added excel system

--- TestWPFApp/MembersUserControl.xaml.cs
+++ TestWPFApp/MembersUserControl.xaml.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Data;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -14,6 +18,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+using Microsoft.Win32;
+using OfficeOpenXml;
+using OfficeOpenXml.Table;
 
 namespace TestWPFApp
 {
@@ -53,7 +60,7 @@ namespace TestWPFApp
             {
                 return;
             }
-            if (txtMail.Text.Length == 0 || password.Length == 0|| txtMail.Text.Length == 0 || txtPhoneNumber.Text.Length == 0)
+            if (txtMail.Text.Length == 0 || password.Length == 0 || txtMail.Text.Length == 0 || txtPhoneNumber.Text.Length == 0)
             {
                 infoText.Visibility = Visibility.Visible; // opening visibility of info text
                 infoText.Content = "Fill all required fields"; // info text message
@@ -107,7 +114,7 @@ namespace TestWPFApp
             {
                 return;
             }
-            if (txtMail.Text.Length == 0 || password.Length == 0  || txtMail.Text.Length == 0 || txtPhoneNumber.Text.Length == 0)
+            if (txtMail.Text.Length == 0 || password.Length == 0 || txtMail.Text.Length == 0 || txtPhoneNumber.Text.Length == 0)
             {
                 infoText.Visibility = Visibility.Visible; // opening visibility of info text
                 infoText.Content = "Fill all required fields"; // info text message
@@ -150,7 +157,7 @@ namespace TestWPFApp
             }
             var item = (sender as FrameworkElement).DataContext;
             membersDataGrid.Items.Remove(item);
-               //DBConnection.ListAllUsers(membersDataGrid);
+            //DBConnection.ListAllUsers(membersDataGrid);
 
         }
 
@@ -160,6 +167,84 @@ namespace TestWPFApp
             (sender as DispatcherTimer).Stop();
             _isTimerRunning = false;
         }
+
+        private void btnExcel_Click(object sender, RoutedEventArgs e)
+        {
+            //// Excel paketi oluştur
+            //ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            //ExcelPackage package = new ExcelPackage();
+            //// Çalışma sayfası oluştur
+            //ExcelWorksheet worksheet = package.Workbook.Worksheets.Add("Sheet1");
+            //// Datagrid verilerini çalışma sayfasına kopyala
+            //for (int i = 0; i < membersDataGrid.Columns.Count; i++)
+            //{
+            //    worksheet.Cells[1, i + 1].Value = membersDataGrid.Columns[i].Header;
+            //}
+            //for (int i = 0; i < membersDataGrid.Items.Count; i++)
+            //{
+            //    for (int j = 0; j < membersDataGrid.Columns.Count; j++)
+            //    {
+            //        var cell = membersDataGrid.Columns[j].GetCellContent(membersDataGrid.Items[i]);
+            //        if (cell is TextBlock textBlock)
+            //        {
+            //            worksheet.Cells[i + 2, j + 1].Value = textBlock.Text;
+            //        }
+            //        else if (cell is System.Windows.Controls.CheckBox checkBox)
+            //        {
+            //            worksheet.Cells[i + 2, j + 1].Value = checkBox.IsChecked;
+            //        }
+            //        // Diğer kontrol türleri için de benzer şekilde yazabilirsiniz
+            //    }
+            //}
+            //// Paketi bir dosya akışına yaz
+            //MemoryStream stream = new MemoryStream();
+            //package.SaveAs(stream);
+            //// Dosya akışını indirmek için bir SaveFileDialog kullan
+            //SaveFileDialog saveDialog = new SaveFileDialog();
+            //saveDialog.Filter = "Excel files (*.xlsx)|*.xlsx";
+            //saveDialog.FileName = "datagrid.xlsx";
+            //if (saveDialog.ShowDialog() == true)
+            //{
+            //    stream.Position = 0;
+            //    using (FileStream fileStream = File.Create(saveDialog.FileName))
+            //    {
+            //        stream.CopyTo(fileStream);
+            //    }
+            //}
+
+            string filePath = "";
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter = "Excel | *.xlsx | Excel 2003 | *.xls";
+            dialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            if (dialog.ShowDialog() == true)
+            {
+                filePath = dialog.FileName;
+            }
+            if (string.IsNullOrEmpty(filePath))
+            {
+                MessageBox.Show("Link is not valid");
+                return;
+            }
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            ObservableCollection<Member> members = new ObservableCollection<Member>();
+            members = (ObservableCollection<Member>)membersDataGrid.ItemsSource;
+            DataTable dt = members.ToDataTable();
+            using (ExcelPackage pck = new ExcelPackage(new FileInfo(filePath)))
+            {
+                ExcelWorksheet ws = pck.Workbook.Worksheets.Add("Data");
+                ws.Cells["A1"].LoadFromDataTable(dt, true);
+                pck.Save();
+            }
+
+
+
+
+
+
+        }
+
+
+
 
     }
 
@@ -172,4 +257,25 @@ namespace TestWPFApp
         public string Password { get; set; }
         public string Email { get; set; }
     }
-}
+    public static class Test
+    {
+        public static DataTable ToDataTable<T>(this ObservableCollection<T> collection)
+        {
+            DataTable dt = new DataTable();
+            PropertyInfo[] props = typeof(T).GetProperties();
+            foreach (var prop in props)
+            {
+                dt.Columns.Add(prop.Name, prop.PropertyType);
+            }
+            foreach (var item in collection)
+            {
+                DataRow row = dt.NewRow();
+                foreach (var prop in props)
+                {
+                    row[prop.Name] = prop.GetValue(item, null);
+                }
+                dt.Rows.Add(row);
+            }
+            return dt;
+        }
+    } }
